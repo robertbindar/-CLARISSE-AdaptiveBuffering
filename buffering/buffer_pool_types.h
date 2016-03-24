6e1c6e1c5bc9a667e313682fa8559bc06c395ac7
robertbindar <robertbindar@gmail.com>
2016-03-24 19:10:19 +0100

    update types

--- buffering/buffer_pool_types.h
+++ buffering/buffer_pool_types.h
@@ -1,8 +1,20 @@
 #pragma once
-#include "stdint.h"
+#include <stdint.h>
+#include <pthread.h>
+#include "uthash.h"
+#include "list.h"
 
 typedef char cls_byte_t;
 typedef uint64_t cls_size_t;
+
+typedef enum
+{
+  UNALLOCATED = -1,
+  READY,
+  BLOCKED_READ,
+  BLOCKED_WRITE,
+  SWAPPED
+} buffer_state_t;
 
 typedef struct
 {
@@ -12,7 +24,31 @@ typedef struct
 
 typedef struct
 {
+  buffer_state_t state;
+
   cls_buf_handle_t handle;
+
+  cls_byte_t *data;
+
+  UT_hash_handle hh;
+
+  pthread_mutex_t lock;
+
+  uint8_t busy;
+  pthread_cond_t buf_busy;
+
+  uint32_t nr_consumers_waiting;
+  uint32_t nr_coll_participants;
 } cls_buf_t;
 
+typedef struct
+{
+  cls_size_t buffer_size;
+
+  cls_size_t buffers_count;
+
+  cls_buf_t *buffers;
+
+  pthread_mutex_t lock;
+} cls_bufferpool_t;
 
--- buffering/cls_buffering.c
+++ buffering/cls_buffering.c
@@ -1,3 +1,19 @@
 #include "cls_buffering.h"
 
+error_code cls_get(cls_buf_handle_t bh, cls_byte_t *data)
+{
+
+}
+
+error_code cls_put(cls_buf_handle_t bh, cls_size_t offset, const cls_byte_t *data,
+                   cls_size_t count)
+{
+
+}
+
+error_code cls_put_all(cls_buf_handle_t bh, cls_size_t offset, const cls_byte_t *data,
+                       cls_size_t count, uint32_t participants)
+{
+
+}
 
